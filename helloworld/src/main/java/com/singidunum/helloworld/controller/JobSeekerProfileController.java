cdee539d404234edd0f1eda5a6db5a4488e349a9
Dusan Stoiljkovic <dusanstoiljkovic02@gmail.com>
2025-09-15 21:59:30 +0200

    Global Serch

--- helloworld/src/main/java/com/singidunum/helloworld/controller/JobSeekerProfileController.java
+++ helloworld/src/main/java/com/singidunum/helloworld/controller/JobSeekerProfileController.java
@@ -1,13 +1,17 @@
 package com.singidunum.helloworld.controller;
-
 import com.singidunum.helloworld.entity.JobSeekerProfile;
 import com.singidunum.helloworld.entity.Skills;
 import com.singidunum.helloworld.entity.Users;
 import com.singidunum.helloworld.repository.UsersRepository;
 import com.singidunum.helloworld.service.JobSeekerProfileService;
+import com.singidunum.helloworld.util.FileDownloadUtil;
 import com.singidunum.helloworld.util.FileUploadUtil;
-import lombok.AllArgsConstructor;
-import org.springframework.data.repository.query.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,10 +19,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -29,32 +30,39 @@ import java.util.Optional;
 
 @Controller
 @RequestMapping("/job-seeker-profile")
-@AllArgsConstructor
 public class JobSeekerProfileController {
 
-    private final JobSeekerProfileService jobSeekerProfileService;
+    private JobSeekerProfileService jobSeekerProfileService;
+
     private UsersRepository usersRepository;
 
+    @Autowired
+    public JobSeekerProfileController(JobSeekerProfileService jobSeekerProfileService, UsersRepository usersRepository) {
+        this.jobSeekerProfileService = jobSeekerProfileService;
+        this.usersRepository = usersRepository;
+    }
 
     @GetMapping("/")
-    public String JobSeekerProfile(Model model) {
+    public String jobSeekerProfile(Model model) {
         JobSeekerProfile jobSeekerProfile = new JobSeekerProfile();
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         List<Skills> skills = new ArrayList<>();
 
-        if(!(auth instanceof AnonymousAuthenticationToken)) {
-            Users user = usersRepository.findByEmail(auth.getName()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            Users user = usersRepository.findByEmail(authentication.getName()).orElseThrow(() -> new UsernameNotFoundException("User not found."));
             Optional<JobSeekerProfile> seekerProfile = jobSeekerProfileService.getOne(user.getUserId());
-            if(seekerProfile.isPresent()) {
+            if (seekerProfile.isPresent()) {
                 jobSeekerProfile = seekerProfile.get();
-                if(jobSeekerProfile.getSkills().isEmpty()) {
+                if (jobSeekerProfile.getSkills().isEmpty()) {
                     skills.add(new Skills());
                     jobSeekerProfile.setSkills(skills);
                 }
             }
+
             model.addAttribute("skills", skills);
             model.addAttribute("profile", jobSeekerProfile);
         }
+
         return "job-seeker-profile";
     }
 
@@ -63,31 +71,31 @@ public class JobSeekerProfileController {
                          @RequestParam("image") MultipartFile image,
                          @RequestParam("pdf") MultipartFile pdf,
                          Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if(!(auth instanceof AnonymousAuthenticationToken)) {
-            Users user = usersRepository.findByEmail(auth.getName()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            Users user = usersRepository.findByEmail(authentication.getName()).orElseThrow(() -> new UsernameNotFoundException("User not found."));
             jobSeekerProfile.setUserId(user);
             jobSeekerProfile.setUserAccountId(user.getUserId());
         }
 
         List<Skills> skillsList = new ArrayList<>();
-        model.addAttribute("skills", skillsList);
         model.addAttribute("profile", jobSeekerProfile);
+        model.addAttribute("skills", skillsList);
 
-        for(Skills skills : jobSeekerProfile.getSkills()) {
+        for (Skills skills : jobSeekerProfile.getSkills()) {
             skills.setJobSeekerProfile(jobSeekerProfile);
         }
 
         String imageName = "";
         String resumeName = "";
 
-        if(!Objects.equals(image.getOriginalFilename(), "")) {
+        if (!Objects.equals(image.getOriginalFilename(), "")) {
             imageName = StringUtils.cleanPath(Objects.requireNonNull(image.getOriginalFilename()));
             jobSeekerProfile.setProfilePhoto(imageName);
         }
 
-        if(!Objects.equals(pdf.getOriginalFilename(), "")) {
+        if (!Objects.equals(pdf.getOriginalFilename(), "")) {
             resumeName = StringUtils.cleanPath(Objects.requireNonNull(pdf.getOriginalFilename()));
             jobSeekerProfile.setResume(resumeName);
         }
@@ -95,19 +103,62 @@ public class JobSeekerProfileController {
         JobSeekerProfile seekerProfile = jobSeekerProfileService.addNew(jobSeekerProfile);
 
         try {
-            String uploadDir = "photos/candidates/"+jobSeekerProfile.getUserAccountId();
-            if(!Objects.equals(image.getOriginalFilename(), "")) {
+            String uploadDir = "photos/candidate/" + jobSeekerProfile.getUserAccountId();
+            if (!Objects.equals(image.getOriginalFilename(), "")) {
                 FileUploadUtil.saveFile(uploadDir, imageName, image);
             }
-
-            if(!Objects.equals(pdf.getOriginalFilename(), "")) {
+            if (!Objects.equals(pdf.getOriginalFilename(), "")) {
                 FileUploadUtil.saveFile(uploadDir, resumeName, pdf);
             }
-        } catch (IOException ex) {
+        }
+        catch (IOException ex) {
             throw new RuntimeException(ex);
         }
+
         return "redirect:/dashboard/";
     }
 
+    @GetMapping("/{id}")
+    public String candidateProfile(@PathVariable("id") int id, Model model) {
 
+        Optional<JobSeekerProfile> seekerProfile = jobSeekerProfileService.getOne(id);
+        model.addAttribute("profile", seekerProfile.get());
+        return "job-seeker-profile";
+    }
+
+    @GetMapping("/downloadResume")
+    public ResponseEntity<?> downloadResume(@RequestParam(value = "fileName") String fileName, @RequestParam(value = "userID") String userId) {
+
+        FileDownloadUtil downloadUtil = new FileDownloadUtil();
+        Resource resource = null;
+
+        try {
+            resource = downloadUtil.getFileAsResource("photos/candidate/" + userId, fileName);
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (resource == null) {
+            return new ResponseEntity<>("File not found", HttpStatus.NOT_FOUND);
+        }
+
+        String contentType = "application/octet-stream";
+        String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
+                .body(resource);
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
